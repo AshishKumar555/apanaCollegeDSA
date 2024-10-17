a9ee586230b7122de4e1fa3e20456969bcea4dc7
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2024-10-17 22:05:51 +0530

    1 decimal to binary was solved

---
+++ 06_BinaryNumberSystem/1decimalToBinary.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int dicimalToBinary(int num){
+    int ans=0;
+    int pow=1;
+    while(num>0){
+     int rem=num%2;
+     num=num/2;
+     ans=ans+(rem*pow);
+     pow=pow*10;
+      
+    }
+    return ans;
+}
+
+
+int main(){
+ int number=5;
+ cout<<dicimalToBinary(number)<<endl;
+    return 0;
+}
