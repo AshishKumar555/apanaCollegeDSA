d3561423c0e8a1967d839e733ff80135dc273b5b
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2024-12-10 19:25:49 +0530

    Pass by reference of array

---
+++ 08_arrays/3.cpp
@@ -0,0 +1,19 @@
+// Find the index of smallest number in an array
+
+#include<iostream>
+using namespace std;
+int main(){
+  int nums[]={10,30,-16,40,-50};
+  int size=5;
+  int smallest=INT32_MAX;
+
+  for(int i=0;i<size;i++){
+    // if(nums[i] < smallest){
+    //   smallest =nums[i];
+    // }
+    smallest =min(nums[i],smallest);
+  }
+
+  cout<<"smallest number is : "<<smallest<<endl;
+    return 0;
+}
---
+++ 08_arrays/4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void changeArr(int brr[],int size){ //it changes the original array of  main function 
+    for(int i=0;i<size;i++){
+        brr[i]=3*brr[i];
+    }
+}
+int main(){
+
+    int arr[]={1,2,3};
+    int size =sizeof(arr)/4;
+    changeArr(arr,size);
+
+    cout<<"After the pass by reference :"<<endl;
+    for(int i=0;i<size;i++){
+     cout<<arr[i]<<" ";
+    }
+    return 0;
+}
