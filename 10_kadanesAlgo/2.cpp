70855abcc59543cbfa25f98800c3cb0776c8971b
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2024-12-20 11:45:35 +0530

    size of array is updated

--- 10_kadanesAlgo/2.cpp
+++ 10_kadanesAlgo/2.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 // Find maximum subArray sum -----------------------------------!
 int main(){
-    int arr[]={1,2,3,4,5};
-    int n=5;
+     int arr[]={1,2,3,4,5};
+    //  int arr[]={3,-4,5,4,-1,7,-8};
+     int size=sizeof(arr)/sizeof(int);
+      cout<<"size of array : "<<size<<endl;
+    int n=size;
     int maxSum=INT32_MIN;
     for(int st=0;st<n;st++){
         int currSum=0;
