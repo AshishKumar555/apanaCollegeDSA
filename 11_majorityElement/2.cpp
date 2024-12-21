6c2703f7b08f0723369fe040cf5043c3062d5854
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2024-12-21 12:25:18 +0530

    Optimised approach is applied

---
+++ 11_majorityElement/2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include<vector>       // Optimised approach
+vector<int> pairSum(vector<int> nums, int target){
+    int n =nums.size();
+    vector<int> ans;
+    int i=0,j=n-1;
+   while(i < j){   //  i<=j not valid (when i==j both point same index that is not a pair )
+    int pairSum = nums[i]+nums[j];
+    if(pairSum == target){
+        ans.push_back(i);
+        ans.push_back(j);
+        return ans;
+    }
+    pairSum > target ? j-- : j++;
+   }
+    return ans;
+}
+
+int main(){
+    vector<int> arr={2,7,11,15};
+    int target = 26;
+    vector<int> ans = pairSum(arr,target);
+    cout<<"pair num is : "<<ans[0]<<", "<<ans[1]<<endl;
+     
+    return 0;
+}
