43f0b710d4d8e6442380ceb069e8d0715d72b225
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2024-12-23 12:15:22 +0530

    optimised approach

---
+++ 11_majorityElement/4.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>                   // TC ---  O(n logn + n)=>  nlogn
+
+int majorityElement(vector<int>& nums) { // optimised approach 
+       int freq =1,ans =nums[0];
+       int n =nums.size();
+       for(int i=1;i<n;i++){
+        if(nums[i] == nums[i-1]){
+            freq++;
+        }
+        else{
+            freq =1,ans =nums[i];
+        }
+        
+        if(freq >n/2){
+          return ans;
+        }
+       }
+       return -1;
+    }
+
+int main(){
+ 
+    vector <int> arr2 ={1,2,2,1,1};
+    sort(arr2.begin(),arr2.end());
+    int ans = majorityElement(arr2);
+    cout<<"Majority Element is : "<<ans<<endl;
+
+    return 0;
+}
+    
