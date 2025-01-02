84641c23c51b2732a0a9d3b314eed1d2f61066df
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2025-01-02 12:02:45 +0530

    Solved buy and sell

---
+++ 13_buySell_stock/3buySell.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+int maxProfit(vector<int>& prices) {
+        int maxProfit =0;
+        int bestBuy =prices[0];
+
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] > bestBuy){
+               maxProfit=max(maxProfit,prices[i]-bestBuy);
+            }
+            
+             bestBuy = min(bestBuy,prices[i]);
+        }
+        return maxProfit;
+        
+    }
+int main(){
+    vector<int> prices={7,1,5,3,6,4};
+    cout<<"Maximum Profit is :"<<maxProfit(prices)<<endl;
+    return 0;
+}
