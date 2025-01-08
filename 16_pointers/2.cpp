a279930179ff7b1f2bc4d3bff68ed2367bf144ae
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2025-01-08 13:09:40 +0530

    2.cpp is added

---
+++ 16_pointers/2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){        //creating pointers  
+    int a=10;
+    int* ptr=&a;
+    cout<<"Address of variable : "<<&a<<endl;
+    cout<<"Pointer val : "<<ptr<<endl;
+    return 0;
+
+}
