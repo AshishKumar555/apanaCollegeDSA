b0f0226fbe7f2a40f8e2568a5e4b916eee45efe4
Ashish Kumar Maurya <ashishvaranasi1001@gmail.com>
2025-01-08 13:06:40 +0530

    address of variable

---
+++ 16_pointers/1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){        //printing address of variable in hexadecimal 
+    int a=10;
+    cout<<"Address of variable : "<<&a<<endl;
+    return 0;
+
+}
